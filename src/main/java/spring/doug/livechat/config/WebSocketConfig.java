94d1cf52795f2f2f9d1c8e97cb713c07fe633c2d
ddouglss <ddouglss1999@gmail.com>
2025-07-20 02:01:12 -0300

    deploy do sistema na aws

--- src/main/java/spring/doug/livechat/config/WebSocketConfig.java
+++ src/main/java/spring/doug/livechat/config/WebSocketConfig.java
@@ -2,11 +2,13 @@ package spring.doug.livechat.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 
 @Configuration
+@EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
@@ -16,6 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/run-livechat-websocket");
+        registry.addEndpoint("/buildrun-livechat-websocket");
     }
 }
